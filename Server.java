f4abc40ecf060287507575e42c12b9bb95455834
Shivani Kushwaha <45357951+shivani2k@users.noreply.github.com>
2021-05-19 17:00:07 +0530

    Update Server.java
    here i added the simple UI like Whats app.

--- Server.java
+++ Server.java
@@ -1,219 +1,195 @@
-package chatting.application;
+import java.awt.BorderLayout;
 
-import javax.swing.*;
-import javax.swing.border.*;
-import java.awt.*;
-import java.awt.event.*;
+import java.awt.EventQueue;
+import java.awt.Image;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JTextArea;
+import java.awt.Color;
 import java.net.*;
 import java.io.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import java.awt.Font;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.JScrollPane;
+import javax.swing.JScrollBar;
 
-import java.util.Calendar;
-import java.text.SimpleDateFormat;
 
-public class Server implements ActionListener{
-    
-    JPanel p1;
-    JTextField t1;
-    JButton b1;
-    static JPanel a1;
-    static JFrame f1 = new JFrame();
-    
-    static Box vertical = Box.createVerticalBox();
-    
-    static ServerSocket skt;
-    static Socket s;
-    static DataInputStream din;
-    static DataOutputStream dout;
-    
-    Boolean typing;
-    
-    Server(){
-        f1.setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        p1 = new JPanel();
-        p1.setLayout(null);
-        p1.setBackground(new Color(7, 94, 84));
-        p1.setBounds(0, 0, 450, 70);
-        f1.add(p1);
-        
-       ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("C:\\Users\\Admin\\Documents\\NetBeansProjects\\chatting applocation\\src\\chatting\\icons\\3.png"));
-       Image i2 = i1.getImage().getScaledInstance(30, 30, Image.SCALE_DEFAULT);
-       ImageIcon i3 = new ImageIcon(i2);
-       JLabel l1 = new JLabel(i3);
-       l1.setBounds(5, 17, 30, 30);
-       p1.add(l1);
-       
-       l1.addMouseListener(new MouseAdapter(){
-           public void mouseClicked(MouseEvent ae){
-               System.exit(0);
-           }
-       });
-       
-       ImageIcon i4 = new ImageIcon(ClassLoader.getSystemResource("chatting/application/icons/1.png"));
-       Image i5 = i4.getImage().getScaledInstance(60, 60, Image.SCALE_DEFAULT);
-       ImageIcon i6 = new ImageIcon(i5);
-       JLabel l2 = new JLabel(i6);
-       l2.setBounds(40, 5, 60, 60);
-       p1.add(l2);
-       
-       ImageIcon i7 = new ImageIcon(ClassLoader.getSystemResource("chatting/application/icons/video.png"));
-       Image i8 = i7.getImage().getScaledInstance(30, 30, Image.SCALE_DEFAULT);
-       ImageIcon i9 = new ImageIcon(i8);
-       JLabel l5 = new JLabel(i9);
-       l5.setBounds(290, 20, 30, 30);
-       p1.add(l5);
-       
-       ImageIcon i11 = new ImageIcon(ClassLoader.getSystemResource("chatting/application/icons/phone.png"));
-       Image i12 = i11.getImage().getScaledInstance(35, 30, Image.SCALE_DEFAULT);
-       ImageIcon i13 = new ImageIcon(i12);
-       JLabel l6 = new JLabel(i13);
-       l6.setBounds(350, 20, 35, 30);
-       p1.add(l6);
-       
-       ImageIcon i14 = new ImageIcon(ClassLoader.getSystemResource("chatting/application/icons/3icon.png"));
-       Image i15 = i14.getImage().getScaledInstance(13, 25, Image.SCALE_DEFAULT);
-       ImageIcon i16 = new ImageIcon(i15);
-       JLabel l7 = new JLabel(i16);
-       l7.setBounds(410, 20, 13, 25);
-       p1.add(l7);
-       
-       
-       JLabel l3 = new JLabel("Gaitonde");
-       l3.setFont(new Font("SAN_SERIF", Font.BOLD, 18));
-       l3.setForeground(Color.WHITE);
-       l3.setBounds(110, 15, 100, 18);
-       p1.add(l3);   
-       
-       
-       JLabel l4 = new JLabel("Active Now");
-       l4.setFont(new Font("SAN_SERIF", Font.PLAIN, 14));
-       l4.setForeground(Color.WHITE);
-       l4.setBounds(110, 35, 100, 20);
-       p1.add(l4);  
-       
-       Timer t = new Timer(1, new ActionListener(){
-           public void actionPerformed(ActionEvent ae){
-               if(!typing){
-                   l4.setText("Active Now");
-               }
-           }
-       });
-       
-       t.setInitialDelay(2000);
-       
-       
-       a1 = new JPanel();
-       a1.setBounds(5, 75, 440, 570);
-       a1.setFont(new Font("SAN_SERIF", Font.PLAIN, 16));
-       f1.add(a1);
-       
-       
-       t1 = new JTextField();
-       t1.setBounds(5, 655, 310, 40);
-       t1.setFont(new Font("SAN_SERIF", Font.PLAIN, 16));
-       f1.add(t1);
-       
-       t1.addKeyListener(new KeyAdapter(){
-           public void keyPressed(KeyEvent ke){
-               l4.setText("typing...");
-               
-               t.stop();
-               
-               typing = true;
-           }
-           
-           public void keyReleased(KeyEvent ke){
-               typing = false;
-               
-               if(!t.isRunning()){
-                   t.start();
-               }
-           }
-       });
-       
-       b1 = new JButton("Send");
-       b1.setBounds(320, 655, 123, 40);
-       b1.setBackground(new Color(7, 94, 84));
-       b1.setForeground(Color.WHITE);
-       b1.setFont(new Font("SAN_SERIF", Font.PLAIN, 16));
-       b1.addActionListener(this);
-       f1.add(b1);
-        
-       f1.getContentPane().setBackground(Color.WHITE);
-       f1.setLayout(null);
-       f1.setSize(450, 700);
-       f1.setLocation(400, 200); 
-       f1.setUndecorated(true);
-       f1.setVisible(true);
-        
-    }
-    
-    public void actionPerformed(ActionEvent ae){
-        try{
-            String out = t1.getText();
+
+public class Server extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6734632686774511121L;
+	private JPanel contentPane;
+	private JTextField text;
+	 static ServerSocket skt;
+	 static Socket s;
+	 static DataInputStream din;
+	 static DataOutputStream dout;
+	 static JTextArea area;
+	  
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Server frame = new Server();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		
+		
+		String msginput ="";
+        try
+        {
+            skt= new ServerSocket(7006);
+            while(true) {
+		            s=skt.accept();
+		            din =new DataInputStream(s.getInputStream());
+		            dout =new DataOutputStream(s.getOutputStream());
+		            while(true) {
             
-            JPanel p2 = formatLabel(out);
-            
-            a1.setLayout(new BorderLayout());
-            
-            JPanel right = new JPanel(new BorderLayout());
-            right.add(p2, BorderLayout.LINE_END);
-            vertical.add(right);
-            vertical.add(Box.createVerticalStrut(15));
-            
-            a1.add(vertical, BorderLayout.PAGE_START);
-            
-            //a1.add(p2);
-            dout.writeUTF(out);
-            t1.setText("");
-        }catch(Exception e){
-            System.out.println(e);
-        }
-    }
-    
-    public static JPanel formatLabel(String out){
-        JPanel p3 = new JPanel();
-        p3.setLayout(new BoxLayout(p3, BoxLayout.Y_AXIS));
-        
-        JLabel l1 = new JLabel("<html><p style = \"width : 150px\">"+out+"</p></html>");
-        l1.setFont(new Font("Tahoma", Font.PLAIN, 16));
-        l1.setBackground(new Color(37, 211, 102));
-        l1.setOpaque(true);
-        l1.setBorder(new EmptyBorder(15,15,15,50));
-        
-        Calendar cal = Calendar.getInstance();
-        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
-        
-        JLabel l2 = new JLabel();
-        l2.setText(sdf.format(cal.getTime()));
-        
-        p3.add(l1);
-        p3.add(l2);
-        return p3;
-    }
-    
-    public static void main(String[] args){
-        new Server().f1.setVisible(true);
-        
-        String msginput = "";
-        try{
-            skt = new ServerSocket(6001);
-            while(true){
-                s = skt.accept();
-                din = new DataInputStream(s.getInputStream());
-                dout = new DataOutputStream(s.getOutputStream());
-            
-	        while(true){
-	                msginput = din.readUTF();
-                        JPanel p2 = formatLabel(msginput);
-                        
-                        JPanel left = new JPanel(new BorderLayout());
-                        left.add(p2, BorderLayout.LINE_START);
-                        vertical.add(left);
-                        f1.validate();
-            	}
-                
+		            msginput = din.readUTF();
+		            area.setText(area.getText() + "\n" + msginput);}
             }
             
-        }catch(Exception e){}
-    }    
+		            
+            
+        }
+        catch (Exception e)
+        {
+           
+            
+        }
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Server() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 533, 504);
+		//setUndecorated(true);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(10, 379, 497, 75);
+		contentPane.add(panel);
+		panel.setLayout(null);
+		
+		text = new JTextField();
+		text.setBounds(10, 11, 367, 53);
+		panel.add(text);
+		text.setColumns(10);
+		
+		JButton btnNewButton = new JButton("Send");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				
+				   try
+			        {
+			            String out =text.getText();
+			            area.setText(area.getText() + "\n\t\t\t" + out);
+			            dout.writeUTF(out);
+			            
+			            text.setText("");
+			        }
+			        catch (Exception ex)
+			        {
+			            
+			        }
+				
+			}
+		});
+		btnNewButton.setBounds(387, 26, 89, 23);
+		panel.add(btnNewButton);
+		
+		 area= new JTextArea();
+		 area.setEditable(false);
+		 area.setWrapStyleWord(true);
+		area.setBackground(Color.WHITE);
+		area.setBounds(10, 102, 497, 266);
+		contentPane.add(area);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(new Color(47, 79, 79));
+		panel_1.setBounds(10, 11, 497, 89);
+		contentPane.add(panel_1);
+		panel_1.setLayout(null);
+		
+		JLabel arrow = new JLabel("gggggg");
+		arrow.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				System.exit(0);
+			}
+		});
+		arrow.setBackground(Color.WHITE);
+		arrow.setBounds(10, 11, 38, 20);
+		panel_1.add(arrow);
+		
+		
+		
+		//arrow.setIcon(new ImageIcon(Server.class.getResource("/icons/3.png")));
+		//arrow.getScaledInstance(24, 24, java.awt.Image.SCALE_SMOOTH);
+		
+		//arrow.setBounds(10, 11, 42, 16);
+		//panel_1.add(arrow);
+		
+		
+		 ImageIcon myimg =new ImageIcon(Server.class.getResource("/icons/3.png"));
+         Image img1=myimg.getImage();
+         Image img2=img1.getScaledInstance(arrow.getWidth(),arrow.getHeight(),Image.SCALE_SMOOTH);
+         ImageIcon i =new ImageIcon(img2);
+         arrow.setIcon(i);
+         
+         JLabel shivani = new JLabel("New label");
+         shivani.setBounds(10, 42, 74, 36);
+         panel_1.add(shivani);
+         
+         
+         ImageIcon myimg1 =new ImageIcon(Server.class.getResource("/icons/WhatsApp Image 2021-05-19 at 1.12.30 PM.jpeg"));
+         Image img11=myimg1.getImage();
+         Image img12=img11.getScaledInstance(shivani.getWidth(),shivani.getHeight(),Image.SCALE_SMOOTH);
+         ImageIcon i1 =new ImageIcon(img12);
+         shivani.setIcon(i1);
+         
+         JLabel name_shivani = new JLabel("Shivani");
+         name_shivani.setForeground(Color.WHITE);
+         name_shivani.setFont(new Font("Tahoma", Font.BOLD, 14));
+         name_shivani.setBounds(182, 17, 90, 26);
+         panel_1.add(name_shivani);
+         
+         JLabel active = new JLabel("Active Now");
+         active.setForeground(Color.WHITE);
+         active.setFont(new Font("Tahoma", Font.BOLD, 13));
+         active.setBounds(167, 54, 105, 24);
+         panel_1.add(active);
+         
+        
+         
+		 
+		
+		 
+	}
 }
